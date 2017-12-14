c1e21c742f1f8493d4c85165774427a29633296a
Payung <payung9@hotmail.com>
2017-12-14 13:59:29 +0700

    ทดสอบ Routes

--- index.php
+++ index.php
@@ -8,31 +8,14 @@ $app = new \Slim\App;
 
 require 'routes/hello.php';
 
+
+
 require 'routes/user.php';
 
 
+require 'routes/dog.php';
 
 
-
-
-$app->get('/dog/post', function () {
-    echo '/dog/post';
-});
-$app->get('/dog/edit', function () {
-    echo '/dog/edit';
-});
-$app->get('/dog/view', function () {
-    echo '/dog/view';
-});
-$app->get('/dog/search', function () {
-    echo '/dog/search';
-});
-$app->get('/dog/history', function () {
-    echo '/dog/history';
-});
-$app->get('/dog/delete', function () {
-    echo '/dog/delete';
-});
 
 
 
---
+++ routes/user.php
@@ -0,0 +1,22 @@
+<?php
+
+$app->get('/user/register', function () {
+    echo '/user/register';
+});
+$app->get('/user/edit', function () {
+    echo '/user/edit';
+});
+$app->get('/user/login', function () {
+    echo '/user/login';
+});
+$app->get('/user/detail', function () {
+    echo '/user/detail';
+});
+$app->get('/user/forgetpassword', function () {
+    echo '/user/forgetpassword';
+});
+$app->get('/user/editpassword', function () {
+    echo '/user/editpassword';
+});
+
+?>
